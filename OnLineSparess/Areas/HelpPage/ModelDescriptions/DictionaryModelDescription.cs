24138273b95c42cdf903472c763ce3e5717508b8
yash_sewp <yashveersewparthab@gmail.com>
2020-03-07 14:56:58 +0200

    new repo push

---
+++ OnLineSparess/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OnLineSparess.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ OnLineSparess/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using OnLineSparess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OnLineSparess.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("name=onlinespares")
+        {
+        }
+        public  DbSet<Entry> Entries { get; set; }
+
+    }
+}
